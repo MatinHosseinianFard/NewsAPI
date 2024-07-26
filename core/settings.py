c61964999d62bbdbe386e16b876a7fd845451b4e
MatinHosseinianFard <matinhosseini795@gmail.com>
2024-07-27 01:14:06 +0330

    Ch3: changed database from sqlite3 to postgresql

--- core/settings.py
+++ core/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
